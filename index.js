26b3c02791551c777a00aab847c5ea2512b5baba
James Koval <jkoval@twitter.com>
2016-04-04 13:23:50 -0400

    better looking output and wildcard for blank fields

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ $(function(){
 
   $("#search-form").on("submit", function(event){
     event.preventDefault();
-    query = $(this).serialize()
-    callJobsApi(query, function(data){
+
+    callJobsApi(getFormQuery(), function(data){
       console.log(data);
       $('#results').empty().mustache('job-template', data);
     });
   })
 
+  function getFormQuery(){
+    return $("#search-form :input")
+      .filter(function(index, element) {
+        // Filter out blank input fields, that act as wildcards (*)
+        return $(element).val() != "";
+      })
+      .serialize();
+  }
+
   function callJobsApi(query, cb){
-    url = 'https://api-v2.themuse.com/jobs?' + query;
+    console.log(query);
+    var url = 'https://api-v2.themuse.com/jobs?' + query;
     $.getJSON(url, cb)
   }
 })
